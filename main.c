76b854850904d66bac8ede3aa2ad7e86e8067c41
minjiwkim <minji5489235790@gmail.com>
2023-09-21 16:43:14 +0900

    4

--- main.c
+++ main.c
@@ -3,12 +3,16 @@
 
 int main(void)
 {
-  char c;
-  
-  printf("enter a character : ");
-  scanf("%c", &c);
-  
-  printf("the next character of %c(%d) is %c(%d)\n", c, c, c+1, c+1);
+    int a;
+    int b;
+    
+    printf("분자를 입력하세요 : ");
+    scanf("%i", &a);
+    
+    printf("분모를 입력하세요 : ");
+    scanf("%i", &b); 
+    
+    printf("나누기의 결과는 %f\n 입니다.", (float)a/b);
     
   system("PAUSE");
   return 0;
