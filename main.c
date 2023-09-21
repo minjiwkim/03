6676b83c6e906975ee8109afe07512f6cefc21a2
minjiwkim <minji5489235790@gmail.com>
2023-09-21 16:23:35 +0900

    3

--- main.c
+++ main.c
@@ -3,15 +3,12 @@
 
 int main(void)
 {
-    char c;
-    int i;
-    
-    printf("input a number : ");
-    scanf("%c", &c);
-    
-    i = c - '0';
-    printf("The input number  ASCII is %i\n", c);
-    printf("The input number is %i\n", i);
+  char c;
+  
+  printf("enter a character : ");
+  scanf("%c", &c);
+  
+  printf("the next character of %c(%d) is %c(%d)\n", c, c, c+1, c+1);
     
   system("PAUSE");
   return 0;
